12d459397262c36de90c1db891dd12eff5d29d58
Ryunosuke Abe <r.abe.dragon@gmail.com>
2015-02-14 16:33:37 +0900

    finish

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,18 +1,68 @@
 #include "ofApp.h"
+#define NUM_OF_POLYGONS 200
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    ofEnableDepthTest();
+    // z軸の前後の判定を直してくれるやーつ。
+    
+    for (int i = 0; i < NUM_OF_POLYGONS ; i++){
+        polygons.push_back(new Polygon3d);
+    }
+    
+    //for debugging
+//    for(int i = 0; i < NUM_OF_POLYGONS ;i++){
+//        cout << i << ": " << polygons.at(i)->mPolygonPosition << endl;
+//    }
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    camera.setPosition(2000*cos((double)ofGetElapsedTimeMillis()/500), 0, 2000*sin((double)ofGetElapsedTimeMillis()/500));
+    camera.lookAt(ofVec3f(0,0,0));
+    
+    //半径1の円において　(x,y) = (cosθ,sinθ);
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    ofBackground(0);
+    ofTranslate(ofGetWidth()/2, ofGetHeight()/2 , 0);
+    
+    camera.begin();
 
+    /*
+    box.setPosition(0, 0, 0);
+    box.set(100);
+    ofSetColor(255);
+    box.drawWireframe();
+    */
+    
+    
+    //Polygon3d polygon1 = Polygon3d();
+    /*
+    polygon1.mPolygonPosition = ofVec3f(100,300,-200);
+    polygon1.mPolygonSize = 300;
+    polygon1.mPolygonType = 0;
+    */
+    //polygon1.draw();
+    
+    for(int i = 0; i < NUM_OF_POLYGONS ; i++){
+        ofSetColor(ofRandom(255), ofRandom(255), ofRandom(255));
+        polygons.at(i)->draw();
+        
+        //ofSetColor(255);
+        //ofLine(ofVec3f(0,0,0), polygons.at(i)->mPolygonPosition);
+        
+        ofSetColor(255,0,0);
+        if(i != 0){
+            ofLine(polygons.at(i-1)->mPolygonPosition,polygons.at(i)->mPolygonPosition);
+        }
+    }
+    
+    camera.end();
+    
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,6 +2,40 @@
 
 #include "ofMain.h"
 
+class Polygon3d{
+public:
+    ofVec3f mPolygonPosition = ofVec3f(0, 0, 0);
+    int mPolygonSize = 0;
+    int mPolygonType = 0;   //0: Box, 1:Sphere
+    
+    Polygon3d(){
+        mPolygonPosition = ofVec3f(ofRandom(-2000,2000),
+                                   ofRandom(-2000,2000),
+                                   ofRandom(-2000,2000));
+        mPolygonSize = floor(ofRandom(10,100));
+        mPolygonType = floor(ofRandom(2));
+        //floor =床 切り捨て ceil = 天井 切り上げ
+    };  //constructorー作るとき
+    ~Polygon3d(){
+    
+    };  //destructorー消すとき
+    
+    void draw() {
+        if(mPolygonType == 0){
+            ofBoxPrimitive boxPremitive;
+            boxPremitive.setPosition(mPolygonPosition);
+            boxPremitive.set(mPolygonSize);
+            boxPremitive.drawWireframe();
+        }else if(mPolygonType==1){
+            //ofSpherePrimitive
+            ofSpherePrimitive spherePrimitive;
+            spherePrimitive.setPosition(mPolygonPosition);
+            spherePrimitive.setRadius(mPolygonSize);
+            spherePrimitive.drawWireframe();
+        }
+    }
+};
+
 class ofApp : public ofBaseApp{
 
 	public:
@@ -18,5 +52,13 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-		
+    
+    ofBoxPrimitive box;
+    ofSpherePrimitive sphere;
+    
+    ofEasyCam easyCom;
+    ofCamera camera;
+    
+    vector <Polygon3d *>polygons;
+
 };
